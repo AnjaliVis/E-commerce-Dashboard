9f0c64118b0f78d0d228c286b84df4340f0bc644
AnjaliVis <49023170+AnjaliVis@users.noreply.github.com>
2023-01-31 15:55:47 +0530

    Initial Commit

---
+++ E-Commerce-Dashboard/front-end/src/components/Nav.js
@@ -0,0 +1,31 @@
+import React, { useEffect } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+
+const Nav = () => {
+    const auth = localStorage.getItem('user');
+    const navigate = useNavigate();
+    const logout = () => {
+        localStorage.clear();
+        navigate('/signup');
+    }
+    return (
+        <div>
+            <img className='logo' src="https://yt3.googleusercontent.com/ytc/AL5GRJWWTjIY5ck502yavPpkWCCrGoPuhaHAhUrMUYVrBg=s900-c-k-c0x00ffffff-no-rj" alt='logo'/>
+            { auth ? <ul className='nav-ul'>
+                <li><Link to="/">Products</Link></li>
+                <li><Link to="/add">Add Product</Link></li>
+                <li><Link to="/update">Update Product</Link></li>
+                <li><Link to="/profile">Profile</Link></li>
+                <li><Link onClick={logout} to="/signup">Logout ({JSON.parse(auth).name})</Link></li>
+            </ul>
+                :
+                <ul className='nav-ul nav-right'>
+                    <li><Link to="/signup">Sign Up</Link></li>
+                    <li><Link to="/login">Login</Link></li>
+                </ul>
+            }
+        </div>
+    )
+}
+
+export default Nav;
